2d91fb8df31b6795fc9aa0ea10be6df5d196d27c
Milton <miltonio94@gmail.com>
2023-05-16 07:14:16 +0000

    PIXIE-5 creating a colour slider

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-port module Main exposing (..)
+port module Main exposing (..) 
 
 import Browser
 import Html exposing (Html)
@@ -9,17 +9,14 @@ import Platform.Cmd as Cmd
 
 type alias Model =
     { lightState : LightState
+    , rgb : RGB
     }
 
 
-type WebSocketMsg
-    = IncomingMsg
-    | OutgoingMsg
-
-
-type alias Message =
-    { contents : String
-    , messageType : WebSocketMsg
+type alias RGB =
+    { r : Maybe Int
+    , g : Maybe Int
+    , b : Maybe Int
     }
 
 
@@ -38,26 +35,6 @@ lightStateToWebSocketMsg lightState =
             "OFF"
 
 
-lightStateToChecked : LightState -> Bool
-lightStateToChecked state =
-    case state of
-        LightOn ->
-            True
-
-        LightOff ->
-            False
-
-
-checkedToLightState : Bool -> LightState
-checkedToLightState isChecked =
-    case isChecked of
-        True ->
-            LightOn
-
-        False ->
-            LightOff
-
-
 toggleLightSwitch : LightState -> LightState
 toggleLightSwitch lightState =
     case lightState of
@@ -72,11 +49,18 @@ type Msg
     = LightSwitched LightState
     | WebSocketSendMsg
     | WebSocketGotMsg String
+    | ColourChage Colour String
+
+
+type Colour
+    = Red
+    | Blue
+    | Green
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model LightOff, Cmd.none )
+    ( Model LightOff (RGB Nothing Nothing Nothing), Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd.Cmd Msg )
@@ -92,22 +76,104 @@ update msg model =
         WebSocketGotMsg _ ->
             ( model, Cmd.none )
 
+        ColourChage colourType colour ->
+            ( updateColour colourType colour model, Cmd.none )
 
-desk_light_switch : Model -> Html Msg
-desk_light_switch model =
-    Html.label
-        [ Attributes.class "desk_light_switch" ]
-        [ Html.text "state of light"
-        , Html.button
-            [ Events.onClick (LightSwitched (toggleLightSwitch model.lightState)) ]
-            [ Html.text (lightStateToWebSocketMsg model.lightState) ]
+
+updateColour : Colour -> String -> Model -> Model
+updateColour colourType colour model =
+    case colourType of
+        Red ->
+            { model
+                | rgb =
+                    updateRgb
+                        (\rgb ->
+                            { rgb
+                                | r = String.toInt colour
+                            }
+                        )
+                        model.rgb
+            }
+
+        Green ->
+            { model
+                | rgb =
+                    updateRgb
+                        (\rgb ->
+                            { rgb
+                                | g = String.toInt colour
+                            }
+                        )
+                        model.rgb
+            }
+
+        Blue ->
+            { model
+                | rgb =
+                    updateRgb
+                        (\rgb ->
+                            { rgb
+                                | b = String.toInt colour
+                            }
+                        )
+                        model.rgb
+            }
+
+
+updateRgb : (RGB -> RGB) -> RGB -> RGB
+updateRgb updater rgb =
+    updater rgb
+
+
+desk_light_switch : LightState -> Html Msg
+desk_light_switch state =
+    Html.div []
+        [ Html.label
+            [ Attributes.class "desk_light_switch" ]
+            [ Html.text "state of light"
+            , Html.button
+                [ Events.onClick (LightSwitched (toggleLightSwitch state)) ]
+                [ Html.text (lightStateToWebSocketMsg state) ]
+            ]
+        ]
+
+
+colourSlider : Colour -> Html Msg
+colourSlider colourType =
+    Html.div
+        []
+        [ Html.input
+            [ Attributes.type_ "range"
+            , Attributes.min "0"
+            , Attributes.max "255"
+            , Events.onInput (ColourChage colourType)
+            ]
+            [ Html.text "" ]
+        ]
+
+
+colourPicker : RGB -> Html Msg
+colourPicker rgb =
+    Html.div
+        []
+        [ colourSlider Red
+        , colourSlider Green
+        , colourSlider Blue
+        ]
+
+
+body : Model -> Html Msg
+body model =
+    Html.div []
+        [ desk_light_switch model.lightState
+        , colourPicker model.rgb
         ]
 
 
 view : Model -> Browser.Document Msg
 view model =
     { title = "Desk light"
-    , body = [ desk_light_switch model ]
+    , body = [ body model ]
     }
 
 
