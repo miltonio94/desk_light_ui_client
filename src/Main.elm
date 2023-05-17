466e280e0e793334239f0ea2a6471341f7c65324
Milton <miltonio94@gmail.com>
2023-05-17 08:02:06 +0000

    Refractored colour picker into it's own module

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,17 @@
-port module Main exposing (..)
+module Main exposing (..)
 
 import Browser
 import Html exposing (Html)
 import Html.Attributes as Attributes
 import Html.Events as Events
+import Module.ColourPicker as ColourPicker
 import Platform.Cmd as Cmd
-import Svg
-import Svg.Attributes as SvgAtt
+import Ports
 
 
 type alias Model =
     { lightState : LightState
-    , rgb : RGB
-    }
-
-
-type alias RGB =
-    { r : String
-    , g : String
-    , b : String
+    , rgb : ColourPicker.RGB
     }
 
 
@@ -51,18 +44,12 @@ type Msg
     = LightSwitched LightState
     | WebSocketSendMsg
     | WebSocketGotMsg String
-    | ColourChage Colour String
-
-
-type Colour
-    = Red
-    | Blue
-    | Green
+    | ColourChage ColourPicker.Colour String
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model LightOff (RGB "0" "0" "0"), Cmd.none )
+    ( Model LightOff ColourPicker.initRgb, Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd.Cmd Msg )
@@ -73,58 +60,13 @@ update msg model =
                 |> update WebSocketSendMsg
 
         WebSocketSendMsg ->
-            ( model, outgoingWebsocketMsg (lightStateToWebSocketMsg model.lightState) )
+            ( model, Ports.outgoingWebsocketMsg (lightStateToWebSocketMsg model.lightState) )
 
-        WebSocketGotMsg _ ->
+        WebSocketGotMsg string ->
             ( model, Cmd.none )
 
         ColourChage colourType colour ->
-            ( updateColour colourType colour model, Cmd.none )
-
-
-updateColour : Colour -> String -> Model -> Model
-updateColour colourType colour model =
-    case colourType of
-        Red ->
-            { model
-                | rgb =
-                    updateRgb
-                        (\rgb ->
-                            { rgb
-                                | r = colour
-                            }
-                        )
-                        model.rgb
-            }
-
-        Green ->
-            { model
-                | rgb =
-                    updateRgb
-                        (\rgb ->
-                            { rgb
-                                | g = colour
-                            }
-                        )
-                        model.rgb
-            }
-
-        Blue ->
-            { model
-                | rgb =
-                    updateRgb
-                        (\rgb ->
-                            { rgb
-                                | b = colour
-                            }
-                        )
-                        model.rgb
-            }
-
-
-updateRgb : (RGB -> RGB) -> RGB -> RGB
-updateRgb updater rgb =
-    updater rgb
+            ( ColourPicker.updateColour colourType colour model, Cmd.none )
 
 
 desk_light_switch : LightState -> Html Msg
@@ -140,77 +82,11 @@ desk_light_switch state =
         ]
 
 
-colourSlider : Colour -> RGB -> Html Msg
-colourSlider colourType rgb =
-    Html.div
-        []
-        [ Html.input
-            [ Attributes.type_ "range"
-            , Attributes.min "0"
-            , Attributes.max "255"
-            , Attributes.value (getValueFromColourType colourType rgb)
-            , Events.onInput (ColourChage colourType)
-            ]
-            [ Html.text "" ]
-        ]
-
-
-colourPicker : RGB -> Html Msg
-colourPicker rgb =
-    Html.div
-        []
-        [ colourShower rgb
-        , colourSlider Red rgb
-        , colourSlider Green rgb
-        , colourSlider Blue rgb
-        ]
-
-
-getValueFromColourType : Colour -> RGB -> String
-getValueFromColourType colour rgb =
-    case colour of
-        Red ->
-            rgb.r
-
-        Green ->
-            rgb.g
-
-        Blue ->
-            rgb.b
-
-
-colourShower : RGB -> Html Msg
-colourShower rgb =
-    Html.div
-        []
-        [ Svg.svg
-            [ SvgAtt.viewBox "0 0 300 300"
-            , SvgAtt.width "255"
-            , SvgAtt.height "255"
-            , SvgAtt.fill (rgbToHtmlRgb rgb)
-            ]
-            [ Svg.rect
-                [ SvgAtt.width "300px"
-                , SvgAtt.height "300px"
-                ]
-                []
-            ]
-        ]
-
-
-rgbToHtmlRgb : RGB -> String
-rgbToHtmlRgb rgb =
-    "rgb(%r, %g, %b)"
-        |> String.replace "%r" rgb.r
-        |> String.replace "%g" rgb.g
-        |> String.replace "%b" rgb.b
-
-
 body : Model -> Html Msg
 body model =
     Html.div []
         [ desk_light_switch model.lightState
-        , colourPicker model.rgb
+        , ColourPicker.colourPicker ColourChage model.rgb
         ]
 
 
@@ -221,6 +97,7 @@ view model =
     }
 
 
+main : Program () Model Msg
 main =
     Browser.document
         { init = init
@@ -232,10 +109,5 @@ main =
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    incomingWebSocketMsg WebSocketGotMsg
-
-
-port incomingWebSocketMsg : (String -> msg) -> Sub msg
-
-
-port outgoingWebsocketMsg : String -> Cmd msg
+    Sub.batch
+        [ Ports.incomingWebSocketMsg WebSocketGotMsg ]
---
+++ src/Module/ColourPicker.elm
@@ -0,0 +1,136 @@
+module Module.ColourPicker exposing (..)
+
+import Html exposing (Html)
+import Html.Attributes as Attributes
+import Html.Events as Events
+import Svg
+import Svg.Attributes as SvgAtt
+
+
+type alias RGB =
+    { r : String
+    , g : String
+    , b : String
+    }
+
+
+type Colour
+    = Red
+    | Blue
+    | Green
+
+
+updateRgb : (RGB -> RGB) -> RGB -> RGB
+updateRgb updater rgb =
+    updater rgb
+
+
+colourSlider : (Colour -> String -> msg) -> Colour -> RGB -> Html msg
+colourSlider messenger colourType rgb =
+    Html.div
+        []
+        [ Html.input
+            [ Attributes.type_ "range"
+            , Attributes.min "0"
+            , Attributes.max "255"
+            , Attributes.value (getValueFromColourType colourType rgb)
+            , Events.onInput (messenger colourType)
+            ]
+            [ Html.text "" ]
+        ]
+
+
+colourPicker : (Colour -> String -> msg) -> RGB -> Html msg
+colourPicker msg rgb =
+    Html.div
+        []
+        [ colourShower rgb
+        , colourSlider msg Red rgb
+        , colourSlider msg Green rgb
+        , colourSlider msg Blue rgb
+        ]
+
+
+getValueFromColourType : Colour -> RGB -> String
+getValueFromColourType colour rgb =
+    case colour of
+        Red ->
+            rgb.r
+
+        Green ->
+            rgb.g
+
+        Blue ->
+            rgb.b
+
+
+colourShower : RGB -> Html msg
+colourShower rgb =
+    Html.div
+        []
+        [ Svg.svg
+            [ SvgAtt.viewBox "0 0 300 300"
+            , SvgAtt.width "255"
+            , SvgAtt.height "255"
+            , SvgAtt.fill (rgbToHtmlRgb rgb)
+            ]
+            [ Svg.rect
+                [ SvgAtt.width "300px"
+                , SvgAtt.height "300px"
+                ]
+                []
+            ]
+        ]
+
+
+rgbToHtmlRgb : RGB -> String
+rgbToHtmlRgb rgb =
+    "rgb(%r, %g, %b)"
+        |> String.replace "%r" rgb.r
+        |> String.replace "%g" rgb.g
+        |> String.replace "%b" rgb.b
+
+
+initRgb : RGB
+initRgb =
+    { r = "0", g = "0", b = "0" }
+
+
+updateColour : Colour -> String -> { rgb | rgb : RGB } -> { rgb | rgb : RGB }
+updateColour colourType colour model =
+    case colourType of
+        Red ->
+            { model
+                | rgb =
+                    updateRgb
+                        (\rgb ->
+                            { rgb
+                                | r = colour
+                            }
+                        )
+                        model.rgb
+            }
+
+        Green ->
+            { model
+                | rgb =
+                    updateRgb
+                        (\rgb ->
+                            { rgb
+                                | g = colour
+                            }
+                        )
+                        model.rgb
+            }
+
+        Blue ->
+            { model
+                | rgb =
+                    updateRgb
+                        (\rgb ->
+                            { rgb
+                                | b = colour
+                            }
+                        )
+                        model.rgb
+            }
