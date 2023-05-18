ea526fd263d451d5e7e81f99824fe025c6b598f0
Milton <miltonio94@gmail.com>
2023-05-18 07:33:11 +0000

    Implemeted queue

--- src/Queue.elm
+++ src/Queue.elm
@@ -1,4 +1,4 @@
-module Queue exposing (..)
+module Queue exposing (Queue, dequeu, enqueue, length, peek)
 
 
 type Queue a
@@ -13,11 +13,73 @@ type alias QueueData a =
     }
 
 
-dequeue : Queue a -> Queue a
-dequeue queue =
+dequeu : Queue a -> Queue a
+dequeu queue =
     case queue of
         Empty ->
             queue
 
         Queue queueData ->
-            Queue queueData
+            case ( headFromQueuDatalist queueData, tailFromQueueDataList queueData ) of
+                ( Just head, Just bodyAndTail ) ->
+                    Queue
+                        { queueData
+                            | queue = bodyAndTail
+                            , head = head
+                        }
+
+                _ ->
+                    Empty
+
+
+enqueue : a -> Queue a -> Queue a
+enqueue item queue =
+    case queue of
+        Empty ->
+            Queue
+                { head = item
+                , tail = item
+                , queue = [ item ]
+                }
+
+        Queue queueData ->
+            Queue
+                { queueData
+                    | tail = item
+                    , queue = queueData.queue ++ [ item ]
+                }
+
+
+length : Queue a -> Int
+length queue =
+    case queue of
+        Empty ->
+            0
+
+        Queue queueData ->
+            List.length queueData.queue
+
+
+peek : Queue a -> Maybe a
+peek queue =
+    case queue of
+        Empty ->
+            Nothing
+
+        Queue queueData ->
+            Just queueData.head
+
+
+tailFromQueueDataList : QueueData a -> Maybe (List a)
+tailFromQueueDataList =
+    .queue
+        >> List.tail
+
+
+headFromQueuDatalist : QueueData a -> Maybe a
+headFromQueuDatalist =
+    .queue >> List.head
+
+
+empty =
+    Empty
